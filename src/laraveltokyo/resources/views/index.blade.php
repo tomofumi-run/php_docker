2cc326866f844c312aa8329a45848d7d4a73de8a
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-04 20:55:15 +0900

    [p]Docker build & up Clear

---
+++ src/laraveltokyo/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LaravelでTodoアプリ</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Todo App</h1>
+        <div class="row text-center">
+            <p>Todo1</p>
+            <p>Todo2</p>
+            <p>Todo3</p>
+        </div>
+    </div>
+</body>
+</html>
